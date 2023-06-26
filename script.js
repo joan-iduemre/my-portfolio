1d7305b5db888c2e91c3506dce3dc46cf9ab20b3
Noble Osinachi <nobleosinachi98@gmail.com>
2023-06-26 14:04:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,52 +16,41 @@ function smoothScroll(e) {
     behavior: 'smooth'
   });
 }
-
-// Form submission
 const contactForm = document.getElementById('contact-form');
+const formStatus = document.getElementById('my-form-status');
 
-contactForm.addEventListener('submit', submitForm);
+contactForm.addEventListener('submit', handleSubmit);
 
-function submitForm(e) {
-  e.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
 
-  // Perform form validation and submit logic here
+  try {
+    const formData = new FormData(event.target);
+    const response = await fetch(event.target.action, {
+      method: event.target.method,
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
 
-
-  var form = document.getElementById("contact-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset()
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
+    if (response.ok) {
+      formStatus.innerHTML = 'Thanks for your submission!';
+      contactForm.reset();
+    } else {
+      const responseData = await response.json();
+      const errorMessage = responseData.errors
+        ? responseData.errors.map(error => error.message).join(', ')
+        : 'Oops! There was a problem submitting your form';
+      formStatus.innerHTML = errorMessage;
     }
-    form.addEventListener("submit", handleSubmit)
+  } catch (error) {
+    formStatus.innerHTML = 'Oops! There was a problem submitting your form';
+  }
   
-  // Clear form fields
   contactForm.reset();
 }
+
 
 // Add active class to the navigation link corresponding to the current section
 window.addEventListener('scroll', highlightNavLink);
