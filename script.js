58f193b38b895525599264bf0b419942ec5ea0e3
Noble Osinachi <nobleosinachi98@gmail.com>
2023-06-26 14:10:38 +0100

    Update AJAX Formspree handling

--- script.js
+++ script.js
@@ -16,8 +16,37 @@ function smoothScroll(e) {
     behavior: 'smooth'
   });
 }
+
+
+
+
 const contactForm = document.getElementById('contact-form');
 const formStatus = document.getElementById('my-form-status');
+ 
+
+function showSuccessMessage(message) {
+  formStatus.innerHTML = message;
+  formStatus.className = 'alert-success';
+  formStatus.style.display = 'block';
+}
+
+function showErrorMessage(message) {
+  formStatus.innerHTML = message;
+  formStatus.className = 'alert-error';
+  formStatus.style.display = 'block';
+}
+
+function hideFormStatus() {
+  formStatus.innerHTML = '';
+
+  // Show the form status element
+  formStatus.style.display = 'block';
+
+  // Hide the form status element after 5 seconds
+  setTimeout(function() {
+    formStatus.style.display = 'none';
+  }, 5000);
+}
 
 contactForm.addEventListener('submit', handleSubmit);
 
@@ -35,17 +64,24 @@ async function handleSubmit(event) {
     });
 
     if (response.ok) {
-      formStatus.innerHTML = 'Thanks for your submission!';
+      // formStatus.innerHTML = 'Thanks for your submission!';
+
+      showSuccessMessage( 'Thanks for your submission!');
+      
       contactForm.reset();
     } else {
       const responseData = await response.json();
       const errorMessage = responseData.errors
         ? responseData.errors.map(error => error.message).join(', ')
         : 'Oops! There was a problem submitting your form';
-      formStatus.innerHTML = errorMessage;
+      // formStatus.innerHTML = errorMessage;
+      showErrorMessage(errorMessage)
     }
   } catch (error) {
-    formStatus.innerHTML = 'Oops! There was a problem submitting your form';
+    // formStatus.innerHTML = 'Oops! There was a problem submitting your form';
+      showErrorMessage( 'Oops! There was a problem submitting your form' )
+
+    
   }
   
   contactForm.reset();
